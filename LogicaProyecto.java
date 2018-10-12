f78309145f70b666972b418c4665b7e4126cd9b5
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2018-10-12 17:45:00 -0600

    Update LogicaProyecto.java

--- LogicaProyecto.java
+++ LogicaProyecto.java
@@ -4,25 +4,27 @@ package proyectoprogramacion;
 public class LogicaProyecto {
     
     
-    public String GanarJugador(String a[][]){
+    public String GanarJugador(String a[][], int jugador){
         
     // Declaracion de Variables
-        String salida = "Gana el jugador ", x= "";
+        String salida = "Gana el jugador ", revisa= "";
+        
         
     //Codigo
-        for (int f = 0; f<a.length;f++)
+        if (jugador == 1)
+            revisa = X;
+        for (int f = 0; f<a.length;f++){
             for (int c = 0; c<a[0].length;c++){
-                if (a[0][0]==x && a[1][0]==x && a[2][0]==x){
-                    salida += "Uno"; 
+                if (a[0][0]==revisa && a[1][0]==revisa && a[2][0]==revisa){
+                    salida += jugador +""; 
                     c= a[0].length-1; 
                 }
-                else  if (a[0][1]==x && a[1][1]==x && a[2][1]==x)
-                    salida += "Uno";
-                else  if (a[0][2]==x && a[1][2]==x && a[2][2]==x)
-                    salida += "Uno";
-                
-                
+                else  if (a[0][1]==jugador && a[1][1]==jugador && a[2][1]==jugador)
+                    salida += jugador +"";
+                else  if (a[0][2]==jugador && a[1][2]==jugador && a[2][2]==jugador)
+                    salida += jugador + "";
             }
+        }
     return salida;
     }
     
