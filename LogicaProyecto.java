344696683b8d6ce247fd67f5fce1eee1bd0cfd88
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2018-10-12 17:32:30 -0600

    Update LogicaProyecto.java

--- LogicaProyecto.java
+++ LogicaProyecto.java
@@ -4,7 +4,7 @@ package proyectoprogramacion;
 public class LogicaProyecto {
     
     
-    public String GanarColunmaX(String a[][]){
+    public String GanarJugador(String a[][]){
         
     // Declaracion de Variables
         String salida = "Gana el jugador ", x= "";
@@ -26,25 +26,4 @@ public class LogicaProyecto {
     return salida;
     }
     
-     public String GanarColunmaO(String a[][]){
-        
-    // Declaracion de Variables
-        String salida = "Gana el jugador ", o = "";
-        
-    //Codigo
-        for (int f = 0; f<a.length;f++)
-            for (int c = 0; c<a[0].length;c++){
-                if (a[0][0]==o && a[1][0]==o && a[2][0]==o){
-                    salida += "Uno"; 
-                    c= a[0].length-1; 
-                }
-                else  if (a[0][1]==o && a[1][1]==o && a[2][1]==o)
-                    salida += "Uno";
-                else  if (a[0][2]==o && a[1][2]==o && a[2][2]==o)
-                    salida += "Uno";
-                
-                
-            }
-    return salida;
-    }
-}
+     
